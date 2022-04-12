f00f00fad048898d82b802d4485e605dc86b3c9d
Iver Punayan <53965169+iverjaycel@users.noreply.github.com>
2022-04-12 22:01:10 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ pipe_lr = joblib.load(open("models/emotion_classifier_pipe_lr_03_june_2021.pkl",
 
 
 # Track Utils
-from track_utils import create_page_visited_table,add_page_visited_details,view_all_page_visited_details,add_prediction_details,view_all_prediction_details,create_emotionclf_table
+#from track_utils import create_page_visited_table,add_page_visited_details,view_all_page_visited_details,add_prediction_details,view_all_prediction_details,create_emotionclf_table
 
 # Fxn
 def predict_emotions(docx):
