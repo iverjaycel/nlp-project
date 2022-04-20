dae02fe4e83f9638f662fff890a7976b83328f2f
Iver Punayan <53965169+iverjaycel@users.noreply.github.com>
2022-04-21 07:45:20 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ from track_utils import create_page_visited_table,add_page_visited_details,view_
 def predict_emotions(docx):
 	results = pipe_lr.predict([docx])
 	return results[0]
-
+	
 def get_prediction_proba(docx):
 	results = pipe_lr.predict_proba([docx])
 	return results
@@ -31,7 +31,7 @@ emotions_emoji_dict = {"anger":"😠","disgust":"🤮", "fear":"😨😱", "happ
 # Main Application
 def main():
 	st.title("Emotion Detection App")
-	menu = ["Home","Monitor","About"]
+	menu = ["Home","About"]
 	choice = st.sidebar.selectbox("Menu",menu)
 	create_page_visited_table()
 	create_emotionclf_table()
@@ -75,29 +75,6 @@ def main():
 				st.altair_chart(fig,use_container_width=True)
 
 
-
-	elif choice == "Monitor":
-		add_page_visited_details("Monitor",datetime.now())
-		st.subheader("Monitor App")
-
-		with st.expander("Page Metrics"):
-			page_visited_details = pd.DataFrame(view_all_page_visited_details(),columns=['Pagename','Time_of_Visit'])
-			st.dataframe(page_visited_details)	
-
-			pg_count = page_visited_details['Pagename'].value_counts().rename_axis('Pagename').reset_index(name='Counts')
-			c = alt.Chart(pg_count).mark_bar().encode(x='Pagename',y='Counts',color='Pagename')
-			st.altair_chart(c,use_container_width=True)	
-
-			p = px.pie(pg_count,values='Counts',names='Pagename')
-			st.plotly_chart(p,use_container_width=True)
-
-		with st.expander('Emotion Classifier Metrics'):
-			df_emotions = pd.DataFrame(view_all_prediction_details(),columns=['Rawtext','Prediction','Probability','Time_of_Visit'])
-			st.dataframe(df_emotions)
-
-			prediction_count = df_emotions['Prediction'].value_counts().rename_axis('Prediction').reset_index(name='Counts')
-			pc = alt.Chart(prediction_count).mark_bar().encode(x='Prediction',y='Counts',color='Prediction')
-			st.altair_chart(pc,use_container_width=True)	
 
 
 
